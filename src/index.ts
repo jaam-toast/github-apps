4a5f1cd7afbf545c50d972e56c221bdd9802b8f0
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-05-12 16:08:48 +0900

    ✨ feat: add push request

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
-import { Context, Probot } from "probot";
+import axios from "axios";
+import jwt from "jsonwebtoken";
+
+import type { Context, Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context: Context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  app.on("push", (context: Context<"push">) => {
+    const repoCloneUrl = context.payload.repository.clone_url;
+    const now = Date.now();
+    const githubAccessToken = jwt.sign(
+      {
+        iat: now,
+        exp: now + 600,
+        iss: 323481,
+      },
+      process.env.PRIVATE_KEY!,
+      { algorithm: "RS256" }
+    );
+    const accessToken = jwt.sign(
+      {
+        githubAccessToken,
+      },
+      process.env.JWT_SECRET_KEY!
+    );
+
+    axios({
+      method: "POST",
+      url: `${process.env.SERVER_URL}/api/projects`,
+      params: {
+        repository: repoCloneUrl,
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
     });
-    await context.octokit.issues.createComment(issueComment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
